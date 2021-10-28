03eaa021b1a43d5f63bc3528a0eaddd4f64bd15a
sangwonme <paulmoguri@naver.com>
2021-10-28 21:51:12 +0900

    :art: Fix front-back structure

--- cardflip.js
+++ cardflip.js
@@ -2,14 +2,27 @@
 var card = document.getElementsByClassName('card')[0];
 var cardFront = document.getElementsByClassName('card__side--front')[0];
 var cardBack = document.getElementsByClassName('card__side--back')[0];
+var icons = document.getElementsByClassName('icon');
 
 // init rotateY
 cardFront.style.transform = 'rotateY(0deg)';
 cardBack.style.transform = 'rotateY(180deg)';
 onFront = true;
+onIconHover = false;
+
+// when hover on icon
+document.querySelectorAll('.icon').forEach(icon => {
+  icon.addEventListener('mouseover', function(){
+    onIconHover = true;
+  });
+  icon.addEventListener('mouseleave', function(){
+    onIconHover = false;
+  });
+});
 
 // when card is onclick, flip the card
 card.addEventListener("click", function(){
+  if(!onIconHover){
     if(onFront){
         cardFront.style.transform = 'rotateY(180deg)';
         cardBack.style.transform = 'rotateY(360deg)';
@@ -18,4 +31,5 @@ card.addEventListener("click", function(){
         cardBack.style.transform = 'rotateY(180deg)';
     }
     onFront = !onFront;
+  }
 });
